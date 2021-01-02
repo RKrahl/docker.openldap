6557ae03be3999a4902d9372d1d316af21e89243
Rolf Krahl <rolf@rotkraut.de>
2021-01-02 21:27:28 +0100

    Simplify things by calling slapd directly, bypassing /usr/lib/openldap/start

--- start-openldap.py
+++ start-openldap.py
@@ -1,34 +1,44 @@
 #! /usr/bin/python3
 
+import os
+import os.path
 from pathlib import Path
+import shutil
 import subprocess
 
-sed = '/usr/bin/sed'
 slapadd = '/usr/sbin/slapadd'
+slapd = '/usr/sbin/slapd'
 
 etc_dir = Path('/etc/openldap')
 tls_dir = etc_dir / 'tls'
 conf_dir = etc_dir / 'slapd.d'
+pid_dir = Path('/run/slapd')
+
+if tls_dir.is_dir():
+    uris = "ldap:/// ldaps:/// ldapi:///"
+else:
+    uris = "ldap:/// ldapi:///"
 
 def init_openldap():
-    sysconf = {
-        'OPENLDAP_CONFIG_BACKEND': 'ldap',
-        'OPENLDAP_START_LDAP': 'yes',
-        'OPENLDAP_START_LDAPI': 'yes',
-    }
-    if tls_dir.is_dir():
-        sysconf['OPENLDAP_START_LDAPS'] = 'yes'
-    sed_cmd = [sed, '-i']
-    for k, v in sysconf.items():
-        sed_cmd.extend(['-e', 's/%s=".*"/%s="%s"/' % (k, k, v)])
-    sed_cmd.append('/etc/sysconfig/openldap')
-    subprocess.run(sed_cmd, check=True)
     init_config = etc_dir / 'slapd.ldif'
     init_cmd = [slapadd, '-n', '0',
                 '-F', str(conf_dir), '-l', str(init_config)]
     subprocess.run(init_cmd, check=True)
 
+def fix_directories():
+    pid_dir.mkdir(mode=0o755, parents=True, exist_ok=True)
+    shutil.chown(pid_dir, user='ldap', group='ldap')
+    for dirpath, dirnames, filenames in os.walk(conf_dir):
+        shutil.chown(dirpath, user='ldap', group='ldap')
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            shutil.chown(filepath, user='ldap', group='ldap')
+
 if not (conf_dir / 'cn=config').is_dir():
     init_openldap()
 
-subprocess.Popen('/usr/lib/openldap/start')
+fix_directories()
+
+slapd_cmd = [slapd, '-h', uris, '-F', str(conf_dir),
+             '-u', 'ldap', '-g', 'ldap', '-o', 'slp=off']
+subprocess.Popen(slapd_cmd)
